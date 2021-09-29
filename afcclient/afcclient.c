613aadf091ea5a4862699687dbb92bbdee6d14f3
Kevin Bradley <kevin.w.bradley@me.com>
2021-09-29 00:03:16 -0700

    trim more frivolous code

--- afcclient/afcclient.c
+++ afcclient/afcclient.c
@@ -407,58 +407,6 @@ int dump_afc_path(afc_client_t afc, const char *path, FILE *outf) {
     return ret;
 }
 
-
-
-//obsolete, was being used when i was trying to convert paths.
-
-/*
- 
- char *str_replace(char *orig, char *rep, char *with) {
- char *result; // the return string
- char *ins;    // the next insert point
- char *tmp;    // varies
- int len_rep;  // length of rep
- int len_with; // length of with
- int len_front; // distance between rep and end of last rep
- int count;    // number of replacements
- 
- if (!orig)
- return NULL;
- if (!rep)
- rep = "";
- len_rep = strlen(rep);
- if (!with)
- with = "";
- len_with = strlen(with);
- 
- ins = orig;
- for (count = 0; tmp = strstr(ins, rep); ++count) {
- ins = tmp + len_rep;
- }
- 
- // first time through the loop, all the variable are set correctly
- // from here on,
- //    tmp points to the end of the result string
- //    ins points to the next occurrence of rep in orig
- //    orig points to the remainder of orig after "end of rep"
- tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
- 
- if (!result)
- return NULL;
- 
- while (count--) {
- ins = strstr(orig, rep);
- len_front = ins - orig;
- tmp = strncpy(tmp, orig, len_front) + len_front;
- tmp = strcpy(tmp, with) + len_with;
- orig += len_front + len_rep; // move to next "end of rep"
- }
- strcpy(tmp, orig);
- return result;
- }
- 
- */
-
 /*
  i think text files writing in binary will not be okay,
  but windows fopen defaults to text, so files get corrupt
